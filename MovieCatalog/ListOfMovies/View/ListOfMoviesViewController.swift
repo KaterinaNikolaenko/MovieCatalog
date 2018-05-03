217db0f34f56321f81afa72816454392da41f944
Katerina <katya-nik@list.ru>
2018-05-04 00:27:12 +0300

    [+] added tableView

--- MovieCatalog/ListOfMovies/View/ListOfMoviesViewController.swift
+++ MovieCatalog/ListOfMovies/View/ListOfMoviesViewController.swift
@@ -11,15 +11,45 @@ import UIKit
 class ListOfMoviesViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
+    let cellReuseIdentifier = "Cell"
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setup()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-
-
+    
+    private func setup() {
+        initialisation()
+        setupNavigation()
+        setupTableView()
+    }
+    
+    private func initialisation() {
+        
+    }
+    private func setupTableView() {
+        
+    }
+    
+    private func setupNavigation() {
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
 }
 
+extension ListOfMoviesViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell:UITableViewCell = self.tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier) as UITableViewCell!
+        cell.textLabel?.text = "aaaa"
+        return cell
+    }
+}
